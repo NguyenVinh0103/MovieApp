7e02975a057151757bc47e1405a0a3d1bdd65a46
NguyenVinh <vinhnqgcs17528@gmail.com>
2022-11-21 15:58:42 +0700

    Update

--- src/component/Tip.js
+++ src/component/Tip.js
@@ -6,7 +6,6 @@ import {TextInput} from 'react-native-gesture-handler';
 const TipComp = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.txtTitle}>{'Name'}</Text>
       <TextInput
         style={styles.tip}
         placeholder="Enter your name"
@@ -19,17 +18,6 @@ const TipComp = () => {
 export const Tip = React.memo(TipComp);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  txtTitle: {
-    color: colors.TEXT,
-    fontSize: normalize(18),
-    lineHeight: normalize(22),
-    fontWeight: '700',
-    marginTop: normalize(16),
-    marginLeft: normalizeHorizontal(20),
-  },
   tip: {
     width: '90%',
     backgroundColor: 'rgba(255, 255, 255, 0.1)',
--- src/screens/Create/index.js
+++ src/screens/Create/index.js
@@ -9,7 +9,9 @@ import {
 import React from 'react';
 import {colors, normalize, normalizeHorizontal} from '../../helper';
 import {IC_BACK, IC_APPLE, IC_GOOGLE} from '../../assets';
-import {Tip} from '../../component'
+import {Tip} from '../../component';
+
+import LinearGradient from 'react-native-linear-gradient';
 
 const Item = [
   {
@@ -30,7 +32,7 @@ const renderItem = ({item, index}) => {
   );
 };
 
-export const Create = () => {
+export const Create = placeHolder => {
   return (
     <View style={styles.container}>
       <View style={styles.title}>
@@ -48,7 +50,27 @@ export const Create = () => {
           contentContainerStyle={styles.flatList}
         />
       </View>
+      <Text style={styles.txtTitle}>{'Name'}</Text>
       <Tip />
+      <Text style={styles.txtTitle}>{'Email'}</Text>
+      <Tip />
+      <Text style={styles.txtTitle}>{'Password'}</Text>
+      <Tip placeHolder="enter your password" />
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Login')}
+        style={styles.btnStart}>
+        <LinearGradient
+          start={{x: 0.0, y: 0.25}}
+          end={{x: 0.5, y: 1.0}}
+          colors={['rgba(255, 114, 42, 0.5)', '#FF722A']}
+          style={styles.linearGradient}>
+          <Text style={styles.txtSignIn}>{'Create Account'}</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+      <Text style={styles.txtBottom}>
+        {'Already have an account? '}
+        <Text style={styles.txtLogin}>{'Login'}</Text>
+      </Text>
     </View>
   );
 };
@@ -84,21 +106,65 @@ const styles = StyleSheet.create({
     width: normalizeHorizontal(160),
     height: normalize(60),
     backgroundColor: 'rgba(255, 255, 255, 0.1)',
-    justifyContent:'center',
-    alignItems:'center',
-    marginTop:normalize(40),
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: normalize(40),
     marginLeft: normalizeHorizontal(10),
     marginRight: normalize(10),
-    borderRadius: normalize(12)
+    borderRadius: normalize(12),
   },
   icFlatList: {
     height: normalize(20),
     aspectRatio: 1,
-    justifyContent:'center'
+    justifyContent: 'center',
   },
   flatList: {
     flexDirection: 'row',
-    justifyContent:'center',
-    alignItems:'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  txtTitle: {
+    color: colors.TEXT,
+    fontSize: normalize(18),
+    lineHeight: normalize(22),
+    fontWeight: '700',
+    marginTop: normalize(16),
+    marginLeft: normalizeHorizontal(20),
+  },
+  linearGradient: {
+    width: '90%',
+    height: normalize(50),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: normalize(12),
+  },
+  btnStart: {
+    width: '90%',
+    height: normalize(50),
+    bottom: 0,
+    marginBottom: normalize(120),
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  txtSignIn: {
+    color: colors.TEXT,
+    fontSize: normalize(18),
+    fontWeight: '700',
+    lineHeight: normalize(20),
+    letterSpacing: 1,
+  },
+  txtBottom: {
+    color: '#9C9C9C',
+    position: 'absolute',
+    bottom: 0,
+    alignSelf: 'center',
+    marginBottom: normalize(60),
+    fontWeight: '700',
+    fontSize: normalize(18),
+  },
+  txtLogin: {
+    color: colors.TEXT,
   },
 });
